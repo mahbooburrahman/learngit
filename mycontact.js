6757c62ee217ab01efa6cf6cb7a2142e0d70f669
mahboob <mahboob.rahman85@gmail.com>
2018-03-23 15:56:18 +0500

    mycontact.js has been addded

---
+++ mycontact.js
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--><html lang="en"><!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+	<title></title>
+
+	<!-- Standard Favicon -->
+	<link rel="icon" type="image/x-icon" href="assets/images//favicon.ico" />
+
+		
+	<!--[if lt IE 9]>
+		<script src="js/html5/respond.min.js"></script>
+    <![endif]-->
+	
+</head>
+
+<body data-offset="200" data-spy="scroll" data-target=".ownavigation">
+	<div class="main-container">
+	
+	</div>
+	
+</body>
+</html>
